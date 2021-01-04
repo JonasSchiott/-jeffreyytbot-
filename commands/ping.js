ef1b312ed867aaa744f3926b64e093039bc23cae
jeffreyJKG <49994449+jeffreyJKG@users.noreply.github.com>
2021-01-05 00:26:40 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
    message.channel.send    
    var botEmbed = new discord.MessageEmbed()
             .setTitle("✅**help**✅")
-            .setDescription};(":ping_pong: Pong! " + (message.createdTimestamp - Date.now()) + "ms")
+            .setDescription(":ping_pong: Pong! " + (message.createdTimestamp - Date.now()) + "ms")
             .setColor("#0000FF")
             .setThumbnail("https://i.scdn.co/image/ab67706c0000bebb12d1a711a79106326fa56b85")  
             .setFooter("© created by JeffreyYT✅#2340", "https://i.scdn.co/image/ab67706c0000bebb12d1a711a79106326fa56b85")
