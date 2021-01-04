c5cdf4a1888594ab7ca4641fdebf8a62be1dc4b7
jeffreyJKG <49994449+jeffreyJKG@users.noreply.github.com>
2021-01-05 00:25:12 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,23 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-   message.channel.send(":ping_pong: Pong! " + (message.createdTimestamp - Date.now()) + "ms")
-}
+   message.channel.send    
+   var botEmbed = new discord.MessageEmbed()
+            .setTitle("✅**help**✅")
+            .setDescription};(":ping_pong: Pong! " + (message.createdTimestamp - Date.now()) + "ms")
+            .setColor("#0000FF")
+            .setThumbnail("https://i.scdn.co/image/ab67706c0000bebb12d1a711a79106326fa56b85")  
+            .setFooter("© created by JeffreyYT✅#2340", "https://i.scdn.co/image/ab67706c0000bebb12d1a711a79106326fa56b85")
+            .setTimestamp();
+
+
+
+            
+            
+            
+
+        return message.channel.send(botEmbed); 
+
 
 module.exports.help = {
    name: "ping",
