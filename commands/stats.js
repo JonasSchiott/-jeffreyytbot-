433a4105c945152872303fcd17c8a7796a122d25
jeffreyJKG <49994449+jeffreyJKG@users.noreply.github.com>
2021-01-08 01:49:01 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -16,15 +16,19 @@ module.exports.run = async (bot, message, args) => {
     let ram = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
   let uptime = ` ${days.toFixed()} days\n ${hours.toFixed()} hours\n ${minutes.toFixed()} min\n ${seconds.toFixed()} sec `;
     const embed = new MessageEmbed()
-      .setTitle("__**Dark Bot Stats**__")
-      .setColor('RANDOM')
+      .setTitle("__✅**STATUS**✅__")
+      .setColor('#0000FF')
       .addField(':clock4: | **Uptime**', `${uptime}`, true)
       .addField(':bar_chart: | **Server Count**', `${bot.guilds.cache.size.toLocaleString()}` , true)
       .addField(':chart_with_upwards_trend: | **User Count**', `${bot.users.cache.size.toLocaleString()}`, true)
       .addField(':card_box: | **Channel Count**', `${bot.channels.cache.size.toLocaleString()}`, true)
       .addField(':chart_with_downwards_trend: | **Memory Usage**', `${ram} MB`, true)
       .addField(':page_facing_up: | **Version**', `v${version}`, true)
-      .setFooter('${message.user.username}')
+      .setThumbnail("https://i.scdn.co/image/ab67706c0000bebb12d1a711a79106326fa56b85")  
+      .setFooter("© created by JeffreyYT✅#2340", "https://i.scdn.co/image/ab67706c0000bebb12d1a711a79106326fa56b85")
+      .setTimestamp();    
+    
+    
     await message.channel.send({ embed });
     
 }
